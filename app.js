158f5bdd9b362f84930e175d47f98768bd66c80e
Thiago Lima <thlima.silva.souza@gmail.com>
2020-03-31 13:55:35 -0300

    implementando roteamento no server para req: /json , http and others

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+
+    const path = req.url;
+
+    if (path === "/json") {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', "text/html");
+        res.end(JSON.stringify({ phrase: "Servidor em JSON" }));
+    } else if (path === "/html") {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', "text/html");
+        const data = fs.readFileSync("/index.html", "utf-8");
+        res.end(data);
+    } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Primeiro Servidor em Nodes');
+    }
 });
 
 server.listen(port, () => {
-  console.log(`Servidor subiu na porta: ${port}/`);
+    console.log(`Servidor subiu na porta: ${port}/`);
 });
